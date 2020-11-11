d1a82532eebe4ae113d47dbc301c8cad52cf9e32
myl7 <myl.ustc@gmail.com>
2020-11-11 22:39:15 +0800

    Add material ui as default transor.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import {parser} from './filters'
 import {Transor} from './transors/types'
+import {MaterialUITransor} from './transors/materialUI'
 
-export default (src: string, transor: Transor): string => {
+export default (src: string, transor?: Transor): string => {
   const nodes = parser(src)
+  transor = transor ? transor : new MaterialUITransor()
   return transor.exec(nodes)
 }
